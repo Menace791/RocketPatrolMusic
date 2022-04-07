6360fcb7560e9fec8e054c3ec019b249c7ca3a5d
Manas Sara <manas.s.sara@gmail.com>
2022-04-07 15:37:23 -0700

    git commit even more rectangles

---
+++ src/scenes/Play.js
@@ -0,0 +1,21 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("play");
+    }
+
+    preload() {
+        this.load.image('starfield', 'assets/starfield.png');
+    }
+
+    create() {
+
+        this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0,0);
+
+        this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);        
+
+        this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(game.config.width - borderUISize, 0, game.config.width, game.config.width, 0xFFFFFF).setOrigin(0,0);    
+    }
+}
