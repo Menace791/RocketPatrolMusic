91ba15a016288d90a2ed7ab829f0117b1de7392e
Manas Sara <manas.s.sara@gmail.com>
2022-04-08 15:44:11 -0700

    Ships can spawn and move

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -6,6 +6,7 @@ class Play extends Phaser.Scene {
     preload() {
         this.load.image('starfield', 'assets/starfield.png');
         this.load.image('rocket', 'assets/rocket.png');
+        this.load.image('ship', 'assets/ship.png');
     }
 
     create() {
@@ -17,31 +18,42 @@ class Play extends Phaser.Scene {
 
         this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0,0);
 
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5,0);
+        this.p1Rocket.reset();
+
+        this.shipA = new Ship(this, 300, 300, 'ship');
+        this.shipB = new Ship(this, 200, 400, 'ship');
+        this.shipC = new Ship(this, 400, 200, 'ship');
+
+
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);        
 
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, game.config.width, game.config.width, 0xFFFFFF).setOrigin(0,0);    
-    
-        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5,0);
-        this.p1Rocket.reset();
     }
 
     update() {
         this.starfield.tilePositionX -= 4;
-        const movementSpeed = 4;
-        if(keyLEFT.isDown) {
-            this.p1Rocket.x -= movementSpeed;
-        }
-        if(keyRIGHT.isDown) {
-            this.p1Rocket.x += movementSpeed;
-        }
 
-        if(Phaser.Input.Keyboard.JustDown(keyF)) {
-            this.p1Rocket.firing = true;
+        const movementSpeed = 4;
+        if (this.p1Rocket.firing == false) {
+            if(keyLEFT.isDown) {
+                this.p1Rocket.x -= movementSpeed;
+            }
+            if(keyRIGHT.isDown) {
+                this.p1Rocket.x += movementSpeed;
+            }
+
+            if(Phaser.Input.Keyboard.JustDown(keyF)) {
+                this.p1Rocket.firing = true;
+            }
         }
 
         this.p1Rocket.update();
+        this.shipA.update();
+        this.shipB.update();
+        this.shipC.update();
     }
 }
