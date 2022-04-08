371aee75adfea31a9642f4baf5ffda8683676e25
Manas Sara <manas.s.sara@gmail.com>
2022-04-08 15:11:34 -0700

    Rocket on screen flying up!

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,11 +5,12 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.image('starfield', 'assets/starfield.png');
+        this.load.image('rocket', 'assets/rocket.png');
     }
 
     create() {
 
-        this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0,0);
+        this.starfield = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'starfield').setOrigin(0,0);
 
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);        
 
@@ -17,5 +18,12 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, game.config.width, game.config.width, 0xFFFFFF).setOrigin(0,0);    
+    
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setOrigin(0.5,0);
+    }
+
+    update() {
+        this.starfield.tilePositionX -= 4;
+        this.p1Rocket.y -= 1;
     }
 }
